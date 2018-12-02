a969507a31ff173deb68747ca97f201faaa75e18
Krists Kreics <kristskreics@gmail.com>
2018-12-02 17:13:16 +0200

    Read edges

--- pseudo.py
+++ pseudo.py
@@ -1,27 +1,20 @@
+# read the file and set the matrixes and variables
 with open("./graphs_part_1/ca-GrQc.txt", "r") as lines:
-  firstrow = True;
+  firstrow = True
   for line in lines:
-    line = line.split();
+    line = line.split()
     if firstrow:
-      graphID = line[1];
-      numOfVertices = int(line[2]);
-      numOfEdges = int(line[3]);
+      graphID = line[1]
+      numOfVertices = int(line[2])
+      numOfEdges = int(line[3])
       k = int(line[4]);
       A = [ [0] * numOfVertices for _ in range(numOfVertices)]
       D = [ [0] * numOfVertices for _ in range(numOfVertices)]
-      firstrow = False;
-    # else:
+      firstrow = False
+    else:
+      A[int(line[0])][int(line[1])] = 1
+      A[int(line[0])][int(line[1])] = 1
+      D[int(line[0])][int(line[0])]
+      D[int(line[1])][int(line[1])]
 
-# print(A, D);
-# open text file
-# read the first line (# graphID numOfVertices numOfEdges k)
-# save k, numOfEdges and graphID
-# create an empty (all 0) numOfVerticesxnumOfVertices adjancency matrix A
-# create an empty (all 0) numOfVerticesxnumOfVertices diagonal matrix D
-# read edges (vertex1ID vertex2ID) line by line until you reach the numOfEdges line
-#   on each read
-#     put 1 in A[vertex1ID][vertex2ID]
-#     put 1 in A[vertex2ID][vertex1ID]
-#     increment D[vertex1ID][vertex1ID] by 1
-#     increment D[vertex2ID][vertex2ID] by 1
-# compute matrix L
+# TODO: compute matrix L
