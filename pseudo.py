8cb82196a76a7ed6949074f90ae80d0ec3eeb028
Timon Schneider <t.d.schneider@protonmail.com>
2018-12-06 00:16:02 +0200

    dynamic networkx plot added

--- pseudo.py
+++ pseudo.py
@@ -1,6 +1,9 @@
 import numpy as np
 import scipy as sp
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import networkx as nx
+from random import random
 
 
 
@@ -66,20 +69,22 @@ np.savetxt("Y.csv", Y, delimiter=",")
 print(output)
 np.savetxt("v.csv", v, delimiter=",")
 
-community_dict = {}
-for idx, i in enumerate(output):
-    community_dict[idx] = i
-#print(community_dict)
 
+
+community_dict = {}
 cutted_edges = np.zeros((k))
 size_coms = np.zeros((k))
-for node in output:
-    size_coms[node] += 1
-# objective function
 samecommunity = 0
 differentcommunity = 0
+
+for idx, i in enumerate(output):
+    community_dict[idx] = i
+    size_coms[i] += 1
+#print(community_dict)
+
+# objective function
 for edge in edges:
-    print(edge)
+    #print(edge)
     unode,vnode = int(edge[0]),int(edge[1])
     ucom,vcom = community_dict[unode],community_dict[vnode]
     if ucom == vcom:
@@ -90,12 +95,34 @@ for edge in edges:
         cutted_edges[vcom] += 1
 print("Total non-cutted edges:",samecommunity)
 print("Total cutted edges:",differentcommunity)
+
 for com in range(k):
     print("Community",com,"cuts",cutted_edges[com],"edges")
 #print(cutted_edges)
-print(size_coms)
+#print(size_coms)
 print("Size of smallest community:",np.min(size_coms))
 
 #OBJECTIVE
 phi = cutted_edges/np.min(size_coms)
 print("Objective:",phi)
+
+
+nodes_dict = {}
+for ka in range(k):
+    k_array = []
+    for idx, node in enumerate(output):
+        if node == ka:
+            k_array.append(idx)
+    nodes_dict[ka] = k_array
+
+colors = [(random(), random(), random()) for _i in range(k)]
+tupleedges = tuple(map(tuple, edges.astype(int)))
+G=nx.Graph()
+for com in range(k):
+    G.add_nodes_from(nodes_dict[com])
+G.add_edges_from(tupleedges)
+
+pos = nx.spring_layout(G)
+for com in range(k):
+    nx.draw(G,pos=pos, nodelist = nodes_dict[com], node_color=colors[com])
+plt.show()
